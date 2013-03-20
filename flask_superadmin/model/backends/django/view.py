324a1b9acb751ba0b2a486ad97d4a3cd186cca85
Stefan Wojcik <wojcikstefan@gmail.com>
2013-03-19 19:48:19 -0700

    exclude full text search + support sqlalchemy

--- flask_superadmin/model/backends/django/view.py
+++ flask_superadmin/model/backends/django/view.py
@@ -51,8 +51,6 @@ class ModelAdmin(BaseModelAdmin):
             return "%s__istartswith" % field_name[1:]
         elif field_name.startswith('='):
             return "%s__iexact" % field_name[1:]
-        elif field_name.startswith('@'):
-            return "%s__search" % field_name[1:]
         else:
             return "%s__icontains" % field_name
 
