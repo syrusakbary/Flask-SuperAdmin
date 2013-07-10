73199bf09de19918d6aa58cfef983e0292a7b80f
Bruno Rocha <rochacbruno@gmail.com>
2013-07-10 03:40:51 -0300

    Better information on fileadmin templates

--- flask_superadmin/contrib/fileadmin.py
+++ flask_superadmin/contrib/fileadmin.py
@@ -357,7 +357,11 @@ class FileAdmin(BaseView):
                 except Exception, ex:
                     flash(gettext('Failed to save file: %(error)s', error=ex))
 
-        return self.render(self.upload_template, form=form)
+        return self.render(self.upload_template,
+                           form=form,
+                           base_path=base_path,
+                           path=path,
+                           msg=gettext(u'Upload a file'))
 
     @expose('/mkdir/', methods=('GET', 'POST'))
     @expose('/mkdir/<path:path>', methods=('GET', 'POST'))
@@ -388,7 +392,10 @@ class FileAdmin(BaseView):
 
         return self.render(self.mkdir_template,
                            form=form,
-                           dir_url=dir_url)
+                           dir_url=dir_url,
+                           base_path=base_path,
+                           path=path,
+                           msg=gettext(u'Create a new directory'))
 
     @expose('/delete/', methods=('POST',))
     def delete(self):
@@ -469,4 +476,5 @@ class FileAdmin(BaseView):
                            form=form,
                            path=op.dirname(path),
                            name=op.basename(path),
-                           dir_url=return_url)
+                           dir_url=return_url,
+                           base_path=base_path)
