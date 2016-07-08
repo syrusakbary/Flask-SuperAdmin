08d9a1b0ac81ebbe1f2d8de55ff49a1256e3ead6
Enrique Piña <epina@hundredrooms.com>
2016-07-08 09:59:17 +0200

    bootstrap 3

--- flask_superadmin/form.py
+++ flask_superadmin/form.py
@@ -167,6 +167,12 @@ class DateTimePickerWidget(widgets.TextInput):
         kwargs['data-role'] = 'datetimepicker'
         return super(DateTimePickerWidget, self).__call__(field, **kwargs)
 
+class TextWidget(widgets.TextInput):
+    def __call__(self, field, **kwargs):
+        kwargs['class'] = 'form-control'
+        return super(TextWidget, self).__call__(field, **kwargs)
+
+
 # def format_form(form):
 #     for field in form:
 #         if isinstance(field,fields.SelectField):
